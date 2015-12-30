f6cab8621eeab009dd4802a82c6fc20be57669c8
Hannah Pinson <pinsonhannah@gmail.com>
2015-12-30 12:02:12 +0100

    implemented mutation dynamics

--- GT/src/gt/Main.java
+++ GT/src/gt/Main.java
@@ -9,47 +9,130 @@ import java.util.Hashtable;
 import java.util.List;
 
 public class Main {
-	static int iterations = 100;
+	static int iterations = 50;
 
 	public static void main(String[] args) {
 
-		/*
-		 * 
-		 * [H] notes on the parameters
-		 * - there are at max. three fractions: modest, fair & greedy, which get a constant demand value per simulation
-		 * 		e.g. there are no multiple modest fractions (with e.g. x_M = 0.1 and x_M = 0.2) in the same simulation
-		 * - fundamentalist population is assumed not to have a fair fraction, so always set initial value to 0
-		 * - fractions should add up to one for each population separately
-		 * - greedy demand is assumed to be the same for both populations
-		 * - modest demand is always 1-greedy demand (automatically assigned for given greedy demand)
-		 * - fair demand is always 0.5 (automatically assigned)
-		 * 
-		 * The parameters that determine the outcome of the simulation are:
-		 * - the greedy demand
-		 * - the initial greedy fundamentalist fraction (since there are only two fractions, the fair one is then also set)
-		 * - the initial greedy non-fundamentalist fraction 
-		 * (- the initial fair non-fundamentalist fraction can be read from the plots of the paper)
-		 */
-
-
 		double greedyDemand = 0.6; 
-		double greedyOrFalsiFundInit = 0.60; //greedy (normal game) or falsifiable (game with falsifiable players) initial fundamentalist fraction 
-		double greedyOrFalsiNonFundInit = 0.05;  //greedy (normal game) or falsifiable (game with falsifiable players) initial non-fundamentalist fraction 
+		double greedyOrFalsiFundInit = 0.50; //greedy (normal game) or falsifiable (game with falsifiable players) initial fundamentalist fraction 
+		double greedyOrFalsiNonFundInit = 0.00;  //greedy (normal game) or falsifiable (game with falsifiable players) initial non-fundamentalist fraction 
 		double fairNonFundInit = 0.50;
+		
+		double petrifiedNonFundInit = 0.0;
+		double terroristFundInit = 0.0;
 
-		double[] fundaIniFractions = {0, 1-greedyOrFalsiFundInit, greedyOrFalsiFundInit}; //! modest initial value = 0 for all fundamentalist populations (since there are no modest players)
+		double[] fundaIniFractions = {0, 0, 1-greedyOrFalsiFundInit-terroristFundInit, greedyOrFalsiFundInit, terroristFundInit}; //! modest initial value = 0 for all fundamentalist populations (since there are no modest players)
 		Population funda = new Population("fundamentalist");
-		double[] nfundaIniFractions = {1-(fairNonFundInit + greedyOrFalsiNonFundInit), fairNonFundInit , greedyOrFalsiNonFundInit}; //first values do not influence sim, but three values should add up to one
+		double[] nfundaIniFractions = {petrifiedNonFundInit, 1-(fairNonFundInit + greedyOrFalsiNonFundInit + petrifiedNonFundInit), fairNonFundInit , greedyOrFalsiNonFundInit, 0}; //first values do not influence sim, but three values should add up to one
 		Population nfunda = new Population("non-fundamentalist");
+		
+		
+		double zero = 0.0; //just a variable that is used at places where the value should always be, logically speaking, zero : )
+		
+		double omega = 0.60; //probability for mutations into terrorist 
 
 
-		// first type of game (box 2)
-		//funda.normalSetup(fundaIniFractions, greedyDemand);
-		//nfunda.normalSetup(nfundaIniFractions, greedyDemand);
+		/***************************************************
+		 * mutation chances for non-fundamentalist population
+		 ***************************************************/
+		Hashtable<String, Hashtable<String, Double>> W = new Hashtable<String, Hashtable<String, Double>>(); 
+		
+		Hashtable<String, Double> mutations_to_petrified = new Hashtable<String, Double>();
+		mutations_to_petrified.put("petrified", 0.0);
+		mutations_to_petrified.put("modest", terroristFundInit);
+		mutations_to_petrified.put("fair", terroristFundInit);
+		mutations_to_petrified.put("greedy", 0.0);
+		mutations_to_petrified.put("terrorist", zero);
+		W.put("petrified", mutations_to_petrified);
+		
+		Hashtable<String, Double> mutations_to_modest = new Hashtable<String, Double>();
+		mutations_to_modest.put("petrified", 0.0);
+		mutations_to_modest.put("modest", 0.0);
+		mutations_to_modest.put("fair", 0.0);
+		mutations_to_modest.put("greedy", 0.0);
+		mutations_to_modest.put("terrorist", zero);
+		W.put("modest", mutations_to_modest);
+		
+		Hashtable<String, Double> mutations_to_fair = new Hashtable<String, Double>();
+		mutations_to_fair.put("petrified", 0.0);
+		mutations_to_fair.put("modest", 0.0);
+		mutations_to_fair.put("fair", 0.0);
+		mutations_to_fair.put("greedy", 0.0);
+		mutations_to_fair.put("terrorist", zero);
+		W.put("fair", mutations_to_fair);
+		
+		Hashtable<String, Double> mutations_to_greedy = new Hashtable<String, Double>();
+		mutations_to_greedy.put("petrified", 0.0);
+		mutations_to_greedy.put("modest", 0.0);
+		mutations_to_greedy.put("fair", 0.0);
+		mutations_to_greedy.put("greedy", 0.0);
+		mutations_to_greedy.put("terrorist", zero);
+		W.put("greedy", mutations_to_greedy);
+		
+		Hashtable<String, Double> empty_terrorist = new Hashtable<String, Double>();
+		empty_terrorist.put("petrified", zero);
+		empty_terrorist.put("modest", zero);
+		empty_terrorist.put("fair", zero);
+		empty_terrorist.put("greedy", zero);
+		empty_terrorist.put("terrorist", zero);
+		W.put("terrorist", empty_terrorist);
+		
+		
+		
+		
+		/***************************************************
+		 * mutation chances for fundamentalist population **
+		 ***************************************************/
+		Hashtable<String, Hashtable<String, Double>> Omega = new Hashtable<String, Hashtable<String, Double>>(); 
+		
+		Hashtable<String, Double> empty_petrified = new Hashtable<String, Double>();
+		empty_petrified.put("petrified", zero);
+		empty_petrified.put("modest", zero);
+		empty_petrified.put("fair", zero);
+		empty_petrified.put("greedy", zero);
+		empty_petrified.put("terrorist", zero);
+		Omega.put("petrified", empty_petrified);
+		
+		Hashtable<String, Double> empty_modest = new Hashtable<String, Double>();
+		empty_modest.put("petrified", zero);
+		empty_modest.put("modest", zero);
+		empty_modest.put("fair", zero);
+		empty_modest.put("greedy", zero);
+		empty_modest.put("terrorist", zero);
+		Omega.put("modest", empty_modest);
+		
+		Hashtable<String, Double> mutations_to_fair_fund = new Hashtable<String, Double>();
+		mutations_to_fair_fund.put("petrified", zero);
+		mutations_to_fair_fund.put("modest", zero);
+		mutations_to_fair_fund.put("fair", 0.0);
+		mutations_to_fair_fund.put("greedy", 0.0);
+		mutations_to_fair_fund.put("terrorist", 0.0);
+		Omega.put("fair", mutations_to_fair_fund);
+			
+		Hashtable<String, Double> mutations_to_greedy_f = new Hashtable<String, Double>();
+		mutations_to_greedy_f.put("petrified", zero);
+		mutations_to_greedy_f.put("modest", zero);
+		mutations_to_greedy_f.put("fair", 0.0);
+		mutations_to_greedy_f.put("greedy", 0.0);
+		mutations_to_greedy_f.put("terrorist", 0.0);
+		Omega.put("greedy", mutations_to_greedy_f);
+		
+		Hashtable<String, Double> mutations_to_terrorist = new Hashtable<String, Double>();
+		mutations_to_terrorist.put("petrified", zero);
+		mutations_to_terrorist.put("modest", zero);
+		mutations_to_terrorist.put("fair", omega);
+		mutations_to_terrorist.put("greedy", omega);
+		mutations_to_terrorist.put("terrorist", 0.0);
+		Omega.put("terrorist", mutations_to_terrorist);
+		
+		
+		// first type of game (box 2) with extension 
+		funda.normalSetup(fundaIniFractions, greedyDemand, Omega);
+		nfunda.normalSetup(nfundaIniFractions, greedyDemand, W); 
 
 		//game with preference falsification (box 3)
-		funda.falsifiableSetup(fundaIniFractions, greedyDemand);
-		nfunda.falsifiableSetup(nfundaIniFractions, greedyDemand);
+		//funda.falsifiableSetup(fundaIniFractions, greedyDemand);
+		//nfunda.falsifiableSetup(nfundaIniFractions, greedyDemand);
 
 
 		Writer writer = null;
@@ -57,17 +140,17 @@ public class Main {
 		try {
 			writer = new BufferedWriter(new OutputStreamWriter(
 					//new FileOutputStream("results_28_12/1d_rhoG0/1d_sigmaG9_rhoG0.txt"), "utf-8"));
-					new FileOutputStream("results_28_12/3b.txt"), "utf-8"));
+					new FileOutputStream("results_extensions/test.txt"), "utf-8"));
 
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		System.out.println("initial values");
+		/*System.out.println("initial values");
 		funda.print();
 		nfunda.print();
-		System.out.println("____________");
+		System.out.println("____________");*/
 
 
 
@@ -83,15 +166,25 @@ public class Main {
 			out += "\n";
 			funda.print();
 			nfunda.print();
-
-
+			
+			
+			//make terrorist attacks (thus the corresponding mutations) dependent on greedy fundamentalist fraction at timestep t:
+			//W_TG (probability for greedy fundamentalist to mutate into terrorist) is already set to omega
+			
+			Hashtable<String, Double> mut_to_petrified = new Hashtable<String, Double>();
+			mut_to_petrified.put("petrified", 0.0);
+			mut_to_petrified.put("modest", funda.fractions.get("terrorist"));
+			mut_to_petrified.put("fair", funda.fractions.get("terrorist"));
+			mut_to_petrified.put("greedy", 0.0);
+			mut_to_petrified.put("terrorist", zero);
+			nfunda.mut_freq.put("petrified", mut_to_petrified);
+			
+			
 			nfunda.prepareUpdate(funda);
 			funda.prepareUpdate(nfunda);
 			nfunda.update(funda);
 			funda.update(nfunda);
-			
-			
-
+		
 
 
 			try {
--- GT/src/gt/Population.java
+++ GT/src/gt/Population.java
@@ -10,31 +10,50 @@ public class Population {
 	Hashtable<String,Double> fractions = new Hashtable<String,Double>();
 	Hashtable<String,Double> demands = new Hashtable<String,Double>();
 	Hashtable<String, Double> expectedFitness = new Hashtable<String,Double>();
+	Hashtable<String, Double> mutation_terms = new Hashtable<String,Double>();
 	double popMeanFitness;
+
+	//mutation frequencies: W_ij is the transition probability per generation for players of type j to mutate to type i
+	Hashtable<String, Hashtable<String, Double>> mut_freq = new Hashtable<String, Hashtable<String, Double>>(); 
 
 	Population(String popType){
 		type = popType;
 	}
 
-	void normalSetup(double[] initFractions, double greedyDemand){
+	void normalSetup(double[] initFractions, double greedyDemand, Hashtable<String, Hashtable<String, Double>> mutation_frequencies){
 
-		fractions.put("modest",initFractions[0]);
-		fractions.put("fair",initFractions[1]);
-		fractions.put("greedy",initFractions[2]);
 
+		fractions.put("petrified",initFractions[0]);
+		fractions.put("modest",initFractions[1]);
+		fractions.put("fair",initFractions[2]);
+		fractions.put("greedy",initFractions[3]);
+		fractions.put("terrorist",initFractions[4]);
+
+		demands.put("petrified", 0.0);
 		demands.put("modest",1-greedyDemand);
 		demands.put("fair", (double) 0.5);
 		demands.put("greedy", greedyDemand);
-		
+		demands.put("terrorist", 1.0);
+
+		expectedFitness.put("petrified", 0.0);
 		expectedFitness.put("modest", 0.0);
 		expectedFitness.put("fair", 0.0);
 		expectedFitness.put("greedy", 0.0);
+		expectedFitness.put("terrorist", 0.0);
 		popMeanFitness = 0;
-		
-		
+
+		mutation_terms.put("petrified", 0.0);
+		mutation_terms.put("modest", 0.0);
+		mutation_terms.put("fair", 0.0);
+		mutation_terms.put("greedy", 0.0);
+		mutation_terms.put("terrorist", 0.0);
+
+		mut_freq = mutation_frequencies;
+
+
 	}
 
-	void falsifiableSetup(double[] initFractions, double greedyDemand){
+	/*void falsifiableSetup(double[] initFractions, double greedyDemand){
 
 		fractions.put("modest",initFractions[0]);
 		fractions.put("fair",initFractions[1]);
@@ -43,33 +62,13 @@ public class Population {
 		demands.put("modest",1-greedyDemand);
 		demands.put("fair", (double) 0.5);
 		demands.put("falsifiable", greedyDemand);
-		
+
 		expectedFitness.put("modest", 0.0);
 		expectedFitness.put("fair", 0.0);
 		expectedFitness.put("falsifiable", 0.0);
 		popMeanFitness = 0;
-	}
-
-	/**
-	 *  	Creates a population of the given initial fractions of strategies, level of greedyness and the type of population.
-	 *
-	 *	@initFraction a double array with the inital fractions (percentages) of the population (modest, fair and greedy)
-	 *	@greedyDemand a double representing the demand of the greedy individuals (what level of control they can accept)
-	 *	@popType a string representing the type of the population
-	 *
-	 */
-	/*Population(double[] initFractions, double greedyDemand, String popType){
-
-		fractions.put("modest",initFractions[0]);
-		fractions.put("fair",initFractions[1]);
-		fractions.put("greedy",initFractions[2]);
-
-		demands.put("modest",1-greedyDemand);
-		demands.put("fair", (double) 0.5);
-		demands.put("greedy", greedyDemand);
-
-		type = popType;
 	}*/
+
 
 
 	/**
@@ -92,7 +91,7 @@ public class Population {
 		if (rowPlayer == "falsifiable" && columnPlayer == "falsifiable"){
 			return 0.5;
 		}
-			
+
 		if (rowPlayer == "falsifiable" && columnPlayer == "fair") //added for clarity, but this is actually superfluent (since x_G + x_F > 1)
 			return 0;
 
@@ -125,25 +124,39 @@ public class Population {
 		return tSum;
 	}
 
-	/**
-	 *	Calculates the mean population fitness of the population distribution ?
-	 *
-	 *	@otherPop the other population in the total population distribution ?
-	 *
-	 *	@return the mean population fitness represented as a double
-	 */
-	/*double populationMeanFitness(Population otherPop){
-		double tSum = 0;
-		Enumeration<String> players = fractions.keys(); //[H] modest, fair, greedy/falsifiable
+
+	void calc_mutation_terms(){
+		System.out.println("mutation terms for " + type + " population");
+		Enumeration<String> players_i = fractions.keys(); 
+		while(players_i.hasMoreElements()){
+			String currentPlayer = players_i.nextElement();
+			double total_mutation_effect = 0;
+			Enumeration<String> players_j = fractions.keys(); 
+			while(players_j.hasMoreElements()){
+				String player_j = players_j.nextElement();
+				//System.out.println("i: " + currentPlayer + ", j: " + player_j + ", W_ij: " + mut_freq.get(currentPlayer).get(player_j));
+				//System.out.println("fraction of j : " + fractions.get(player_j));
+				total_mutation_effect += mut_freq.get(currentPlayer).get(player_j)*fractions.get(player_j)  - mut_freq.get(player_j).get(currentPlayer)*fractions.get(currentPlayer);
+			}	
+			//System.out.println("Total mutation term for fraction " + currentPlayer + ": " + total_mutation_effect);
+			mutation_terms.put(currentPlayer, total_mutation_effect);
+		}
+	}
+	
+	void prepareUpdate(Population otherPop){
+		Enumeration<String> players = expectedFitness.keys(); //[H] modest, fair, greedy/falsifiable
+		double meanFitnessSum = 0;
 		while(players.hasMoreElements()){
 			String currentPlayer = players.nextElement();
-			//System.out.println("Current player: " + currentPlayer);
-			//System.out.println(demands.get(currentPlayer));
-			tSum += fractions.get(currentPlayer) * expectedFitness(currentPlayer, otherPop);
+			double expecFit = expectedFitness(currentPlayer, otherPop);
+			expectedFitness.put(currentPlayer, expecFit);
+			//System.out.println("expected fitness " + expecFit );
+			meanFitnessSum += fractions.get(currentPlayer) * expecFit;
 		}
-		System.out.println("population mean fitness ( " + type + ", " + otherPop.type + " ) = " + tSum);
-		return tSum;
-	}*/
+		popMeanFitness = meanFitnessSum;
+		//calc_mutation_terms();
+		//System.out.println("population mean fitness ( " + type + ", " + otherPop.type + " ) = " + popMeanFitness);
+	}
 
 	/**
 	 *	Calculates the next proportion of individuals choosing the action corresponding to the given fractionKey in the population
@@ -160,49 +173,23 @@ public class Population {
 
 
 	/**
-	 *	:)
-	 */
-	/*Hashtable<String, Double> copyFractions(){
-		Hashtable<String,Double> copiedFractions = new Hashtable<String,Double>();
-		if (fractions.containsKey("greedy"))
-			copiedFractions.put("greedy", fractions.get("greedy"));
-		if (fractions.containsKey("falsifiable"))
-			copiedFractions.put("falsifiable", fractions.get("falsifiable"));
-		copiedFractions.put("modest", fractions.get("modest"));
-		copiedFractions.put("fair", fractions.get("fair"));
-		return copiedFractions;	
-	}*/
-	
-	/**
-	 * 
-	 */
-	
-	void prepareUpdate(Population otherPop){
-		Enumeration<String> players = expectedFitness.keys(); //[H] modest, fair, greedy/falsifiable
-		double meanFitnessSum = 0;
-		while(players.hasMoreElements()){
-			String currentPlayer = players.nextElement();
-			double expecFit = expectedFitness(currentPlayer, otherPop);
-			expectedFitness.put(currentPlayer, expecFit);
-			//System.out.println("expected fitness " + expecFit );
-			meanFitnessSum += fractions.get(currentPlayer) * expecFit;
-		}
-		popMeanFitness = meanFitnessSum;
-		//System.out.println("population mean fitness ( " + type + ", " + otherPop.type + " ) = " + popMeanFitness);
-	}
-
-
-	/**
 	 *	Updates the distributions for all individuals in this population with interactions with the other given population.
 	 *
 	 *	@otherPop the other population in the population distribution ?
 	 */
 	void update(Population otherPop){
-		Enumeration<String> players = fractions.keys(); //[H] modest, fair, greedy/falsifiable
+		Enumeration<String> players = fractions.keys(); 
 		while(players.hasMoreElements()){
 			String currentPlayer = players.nextElement();
 			double updatedFraction = updateFraction(currentPlayer);
 			fractions.put(currentPlayer, updatedFraction);
+		}
+		calc_mutation_terms(); //this update order keeps all fractions normalised (i.e. adding up to one within one population)
+		Enumeration<String> players2 = fractions.keys(); 
+		while(players2.hasMoreElements()){
+			String currentPlayer = players2.nextElement();
+			double added_mutation = fractions.get(currentPlayer) + mutation_terms.get(currentPlayer);
+			fractions.put(currentPlayer, added_mutation);
 		}
 	}
 
@@ -222,14 +209,15 @@ public class Population {
 	 *	Prints relevant data to the console.
 	 */
 	void print(){
-		System.out.println("Current values of the " + type + " population are:");
+		System.out.println("------------Current values of the " + type + " population are:");
+		System.out.println("Petrified fraction: " + round(fractions.get("petrified")) + " with demand: " + demands.get("petrified"));
 		System.out.println("Modest fraction: " + round(fractions.get("modest")) + " with demand: " + demands.get("modest"));
 		System.out.println("Fair fraction: " + round(fractions.get("fair")) + " with demand: " + demands.get("fair"));
 		if (fractions.containsKey("greedy"))
 			System.out.println("Greedy fraction: " + round(fractions.get("greedy")) + " with demand: " + demands.get("greedy"));	
 		if (fractions.containsKey("falsifiable"))
 			System.out.println("Falsifiable fraction: " + round(fractions.get("falsifiable")) + " with demand: " + demands.get("falsifiable"));	
-			
+		System.out.println("Terrorist fraction: " + round(fractions.get("terrorist")) + " with demand: " + demands.get("terrorist"));
 	}
 
 	/**
@@ -240,9 +228,9 @@ public class Population {
 	public String filePrint() {
 		String out= "";
 		if (fractions.containsKey("greedy"))
-			out += round(fractions.get("modest")) +", " + round(fractions.get("fair"))+ ", " + round(fractions.get("greedy"));
+			out += round(fractions.get("petrified")) +", " + round(fractions.get("modest")) +", " + round(fractions.get("fair"))+ ", " + round(fractions.get("greedy"))+ ", " + round(fractions.get("terrorist"));
 		if (fractions.containsKey("falsifiable"))
-			out += round(fractions.get("modest")) +", " + round(fractions.get("fair"))+ ", " + round(fractions.get("falsifiable"));
+			out += round(fractions.get("petrified")) +", " + round(fractions.get("modest")) +", " + round(fractions.get("fair"))+ ", " + round(fractions.get("falsifiable"));
 		return  out;
 	}
 }
